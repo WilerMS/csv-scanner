30bcb652b90a1c9b42eb8509599bcb5836d574dd
WilerMS <wilersoriano@gmail.com>
2024-04-26 23:31:09 +0200

    add server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,12 @@
-console.log('first')
+import { PORT } from '@/constants/env'
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('HELLO WORLD')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`)
+})
