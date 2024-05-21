64579ff89ec6d876be5ad008b35a3e1eed651e51
WilerMS <wilersoriano@gmail.com>
2024-05-21 11:10:13 +0200

    set fileId on init app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css'
+
+const parsedParams = new URLSearchParams(window.location.search)
+const queryParams = parsedParams.toString()
+
+const newPath = queryParams.includes('fileId')
+  ? `?${queryParams}`
+  : `?fileId=${crypto.randomUUID()}&${queryParams}`
+
+window.history.replaceState({}, '', newPath)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
