44d6a462f722240751a7e22ef5e30864e7f21c09
WilerMS <wilersoriano@gmail.com>
2024-05-21 11:11:32 +0200

    refactor file uploader

--- frontend/src/components/FileUploader.tsx
+++ frontend/src/components/FileUploader.tsx
@@ -1,33 +1,51 @@
-import React, { type FC } from 'react'
+import { type FC } from 'react'
 
-interface FileUploaderProps {
-  onSelectFile: (file: File) => void
+import csvIcon from '@/assets/csv_icon.png'
+
+interface Props {
+  onSelectFile?: (file: File) => void
 }
 
-const FileUploader: FC<FileUploaderProps> = ({ onSelectFile }) => {
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.currentTarget.files
-    if (!files) return
-    onSelectFile(files[0])
+export const FileUploader: FC<Props> = ({
+  onSelectFile = () => { }
+}) => {
+  const handleChangeFile = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    const newFile = e.currentTarget.files?.[0]
+
+    if (newFile) onSelectFile?.(newFile)
   }
 
   return (
-    <div className='transition hover:scale-105 hover:shadow-2xl'>
-      <label
-        htmlFor="file"
-        className='text-2xl uppercase cursor-pointer py-5 px-8 rounded-3xl text-white bg-primary-700'
-      >
-        Sube tu archivo CSV
-      </label>
-      <input
-        id='file'
-        type="file"
-        className='hidden'
-        accept='text/csv'
-        onChange={handleChange}
-      />
+    <div>
+      <div className='header flex flex-col gap-[5px]'>
+        <h1 className='text-center text-[20px]'>Upload your CSV</h1>
+        <h3 className='text-[#828282] text-[12px] text-center'>File should be .csv</h3>
+      </div>
+
+      <div className='rounded-[12px] w-full h-[235px] mt-[20px]'>
+        <div className='relative w-full h-full rounded-[8px] border-[#81da8a68] border-[2px] border-dashed overflow-hidden'>
+          <div className='relative group w-full h-full center flex-col'>
+            <div className='w-full h-full center flex-col bg-[#F6F8FB]'>
+              <img
+                src={csvIcon}
+                alt="csv logo icon"
+                className='w-[100px]'
+                style={{
+                  filter: 'grayscale(80%)'
+                }}
+              />
+              <span className='text-[12px] text-[#BDBDBD] mt-[30px]'>Drag & Drop your file here</span>
+            </div>
+            <input
+              className='absolute opacity-0 w-full h-full top-0 left-0 cursor-pointer'
+              type="file"
+              accept='.csv'
+              onChange={handleChangeFile}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
-
-export default FileUploader
